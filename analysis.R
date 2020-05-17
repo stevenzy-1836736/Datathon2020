763b621f0547b56eb635eb5e6779774c8438f9c3
Zhenyu <stevenzy@uw.edu>
2020-05-16 22:44:23 -0700

    modifed

--- analysis.R
+++ analysis.R
@@ -7,3 +7,28 @@ library(styler)
 
 store <- read.csv("grocery_store_data_cleaned.csv")
 
+
+# Unique product names
+product_name <- unique(store$NAME)
+
+
+# Units, reference, name, unit_pricesell/buy, category, total_pricesell/buy, margin, profit
+name_col <- store %>%
+  filter(REFERENCE == 1079) %>%
+  select(NAME)
+
+profit_col <- store %>%
+  filter(REFERENCE == 1079) %>%
+  select(PROFIT)
+
+product_info <- list(name = name_col, profit = profit_col)
+
+
+
+
+function1 <- function(ref){
+  name <- paste0("product", ref, "_info")
+  name <- list(name = name_col, profit = profit_col)
+}
+
+
