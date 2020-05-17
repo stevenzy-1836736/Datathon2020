4feafb28a4f88dfb7127af1d6f9abb62a47bbfee
Zhenyu <stevenzy@uw.edu>
2020-05-16 22:03:28 -0700

    added 2 files

---
+++ analysis.R
@@ -0,0 +1,9 @@
+install.packages("dplyr")
+install.packages("knitr")
+install.packages("styler")
+library(dplyr)
+library(knitr)
+library(styler)
+
+store <- read.csv("grocery_store_data_cleaned.csv")
+
