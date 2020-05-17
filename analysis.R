353eecb87b5038ebe4a66cb045cfbf585dac65e2
Zhenyu <stevenzy@uw.edu>
2020-05-16 23:18:33 -0700

    a

--- analysis.R
+++ analysis.R
@@ -21,14 +21,32 @@ profit_col <- store %>%
   filter(REFERENCE == 1079) %>%
   select(PROFIT)
 
+
+
+#####
+
 product_info <- list(name = name_col, profit = profit_col)
 
+var <- for(i in 1:5){
+    store %>%
+    filter(NAME == product_name[i]) %>%
+    
+  #create_list(product_name[i])
+}
+
+sample_product <- store %>%
+  filter(REFERENCE == 1079)
+sample_profit <- select(sample_product, PROFIT)
+sample_total_sell <- select(sample_product, TOTAL_PRICESELL)
+sample_unit <- select(sample_product, UNITS)
+
+sample_plot <- ggplot(data = sample_product) +
+  geom_point(mapping = aes(x = UNIT_PRICESELL, y = ))
 
 
-
-function1 <- function(ref){
-  name <- paste0("product", ref, "_info")
-  name <- list(name = name_col, profit = profit_col)
+create_list <- function(name, df){
+  result <- paste0(name, "_info")
+  result <- list(profit = profit_col)
 }
 
 
